52c813bcfdbbbff7dfd51f55f13e5299a65eab6e
Nicodannic <lesjeuxvideodannic@gmail.com>
2026-07-26 12:46:20 +0200

    add Vinted photo enhancement

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import Response
-from PIL import Image
+from rembg import remove, new_session
+from PIL import Image, ImageEnhance
 from io import BytesIO
+import cv2
+import numpy as np
+
 
 app = FastAPI()
+
+session = new_session("u2netp")
 
 
 @app.get("/")
@@ -16,24 +22,44 @@ def home():
 @app.post("/remove-background")
 async def remove_background(file: UploadFile = File(...)):
 
-    print("Image reçue")
-
     image_bytes = await file.read()
 
     image = Image.open(
         BytesIO(image_bytes)
+    ).convert("RGBA")
+
+
+    # Suppression fond
+    result = remove(
+        image,
+        session=session
     )
 
-    print("Image ouverte")
 
+    # Amélioration luminosité
+    rgb = result.convert("RGB")
+
+    enhancer = ImageEnhance.Brightness(rgb)
+    rgb = enhancer.enhance(1.08)
+
+
+    enhancer = ImageEnhance.Contrast(rgb)
+    rgb = enhancer.enhance(1.15)
+
+
+    # Netteté
+    enhancer = ImageEnhance.Sharpness(rgb)
+    rgb = enhancer.enhance(1.25)
+
+
+    # Retour PNG
     output = BytesIO()
 
-    image.save(
+    rgb.save(
         output,
         format="PNG"
     )
 
-    print("Image renvoyée")
 
     return Response(
         content=output.getvalue(),
