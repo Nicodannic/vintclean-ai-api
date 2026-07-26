fdfdfca18a60f5d457cc608667f58a9bfc3d972a
Nicodannic <lesjeuxvideodannic@gmail.com>
2026-07-26 12:36:30 +0200

    test api without ai

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import Response
-from rembg import remove, new_session
 from PIL import Image
 from io import BytesIO
 
 app = FastAPI()
-
-session = None
 
 
 @app.get("/")
@@ -19,49 +16,26 @@ def home():
 @app.post("/remove-background")
 async def remove_background(file: UploadFile = File(...)):
 
-    global session
+    print("Image reçue")
 
-    try:
-        print("Image reçue")
+    image_bytes = await file.read()
 
-        if session is None:
-            print("Chargement modèle u2netp...")
-            session = new_session("u2netp")
-            print("Modèle chargé")
+    image = Image.open(
+        BytesIO(image_bytes)
+    )
 
-        image_bytes = await file.read()
+    print("Image ouverte")
 
-        print("Taille :", len(image_bytes))
+    output = BytesIO()
 
-        image = Image.open(
-            BytesIO(image_bytes)
-        )
+    image.save(
+        output,
+        format="PNG"
+    )
 
-        print("Image ouverte")
+    print("Image renvoyée")
 
-        result = remove(
-            image,
-            session=session
-        )
-
-        print("Fond supprimé")
-
-        output = BytesIO()
-
-        result.save(
-            output,
-            format="PNG"
-        )
-
-        print("PNG créé")
-
-        return Response(
-            content=output.getvalue(),
-            media_type="image/png"
-        )
-
-    except Exception as e:
-        print("ERREUR :", e)
-        return {
-            "error": str(e)
-        }
+    return Response(
+        content=output.getvalue(),
+        media_type="image/png"
+    )
